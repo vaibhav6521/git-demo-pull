06b11c8f7e9708475add1257bcd28feb94187b14
vaibhav6521 <vaibhavaatkare6521@gmail.com>
2023-08-29 18:00:49 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { of, map, catchError } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CatchError';
+
+  ngOnInit() {
+    of(1, 2, 3, 4, 5)
+      .pipe(
+        map(n => {
+          if (n === 4) {
+            throw 'four!';
+          }
+          return n;
+        }),
+        catchError(err => of('I', 'II', 'III', 'IV', 'V'))
+      )
+      .subscribe(x => console.log(x));
+    // 1, 2, 3, I, II, III, IV, V
+  }
+}
